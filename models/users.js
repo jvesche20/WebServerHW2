fe9470b7baf956bfe3b755e16e1c969e46179986
jvesche20 <70725626+jvesche20@users.noreply.github.com>
2021-03-04 17:43:47 -0500

    Create users.js

---
+++ models/users.js
@@ -0,0 +1,42 @@
+const Mongoose = require('mongoose');
+
+require('bob-mongoose-currency').loadType(Mongoose);
+
+module.export = Mongoose.model('User', new Mongoose.Schema({
+
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 130,
+  },
+  ssn: {
+    type: String,
+    required: true,
+    unique: true,
+    min: 9,
+    max: 9,
+
+  },
+  address: {
+    type: String,
+    required: false,
+  },
+  phoneNumber: {
+    type: String,
+    required: false,
+  },
+  toJSON: {
+    getters: true,
+    virtuals: false,
+  },
+
+}));
