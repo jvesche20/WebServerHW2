7cf8ddb54b9cbdbcd21fbc10e6804099be99724c
jvesche20 <70725626+jvesche20@users.noreply.github.com>
2021-04-01 20:30:38 -0400

    Middleware delete status

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const Express = require('express');
 const BodyParser = require('body-parser');
 
+const DeleteStatus = require('./middleware/deleteStatus');
+
+const app = Express();
+
+app.use(BodyParser.json());
+app.use(DeleteStatus);
+
 app.listen(8080);
